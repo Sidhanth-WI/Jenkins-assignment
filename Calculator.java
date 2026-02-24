469eb08f30a7ba2a434521786c07bb19b474e2e2
Sidhanth-WI <sidhanth.naik@wissen.com>
2026-02-24 19:13:32 +0530

    Fixed class syntax

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,4 @@
-public Calculator
+public class Calculator
 {
  public int add(int a,int b)
 {
--- CalculatorMain.java
+++ CalculatorMain.java
@@ -1,4 +1,4 @@
-public CalculatorMain
+public class CalculatorMain
 {
 	public static void main(String args[])
 {
